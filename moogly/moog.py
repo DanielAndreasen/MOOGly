758f68ce9a8d801f91137a995c4d5a058f944f59
DanielAndreasen <daniel.andreasen@hotmail.com>
2019-05-05 11:48:40 +0200

    Revert usage of f-string

--- moogly/moog.py
+++ moogly/moog.py
@@ -23,38 +23,38 @@ class MOOG:
         self._run()
 
     def _create_config(self):
-        fout = f'{self.driver}\n'
-        fout += f"terminal     {self.kwargs.get('terminal', 'x11')}\n"
-        fout += f"atmosphere   {self.kwargs.get('atmosphere', '1')}\n"
-        fout += f"molecules    {self.kwargs.get('molecules', '2')}\n"
-        fout += f"trudamp      {self.kwargs.get('trudamp', '1')}\n"
-        fout += f"lines        {self.kwargs.get('lines', '1')}\n"
-        fout += f"flux/int     {self.kwargs.get('fluxint', '0')}\n"
-        fout += f"damping      {self.kwargs.get('damping', '0')}\n"
-        fout += f"plot         {self.kwargs.get('plot')}\n"
-        fout += f"units        {self.kwargs.get('units', '0')}\n"
-        fout += f"model_in     {self.kwargs.get('model_in', 'atm.mod')}\n"
-        fout += f"lines_in     {self.kwargs.get('lines_in', 'lines.moog')}\n"
-        fout += f"summary_out  {self.kwargs.get('summary_out', 'summary.out')}\n"
-        fout += f"standard_out {self.kwargs.get('standard_out', 'standard.out')}\n"
+        fout = '{}\n'.format(self.driver)
+        fout += "terminal     {}\n".format(self.kwargs.get('terminal', 'x11'))
+        fout += "atmosphere   {}\n".format(self.kwargs.get('atmosphere', '1'))
+        fout += "molecules    {}\n".format(self.kwargs.get('molecules', '2'))
+        fout += "trudamp      {}\n".format(self.kwargs.get('trudamp', '1'))
+        fout += "lines        {}\n".format(self.kwargs.get('lines', '1'))
+        fout += "flux/int     {}\n".format(self.kwargs.get('fluxint', '0'))
+        fout += "damping      {}\n".format(self.kwargs.get('damping', '0'))
+        fout += "plot         {}\n".format(self.kwargs.get('plot'))
+        fout += "units        {}\n".format(self.kwargs.get('units', '0'))
+        fout += "model_in     {}\n".format(self.kwargs.get('model_in', 'atm.mod'))
+        fout += "lines_in     {}\n".format(self.kwargs.get('lines_in', 'lines.moog'))
+        fout += "summary_out  {}\n".format(self.kwargs.get('summary_out', 'summary.out'))
+        fout += "standard_out {}\n".format(self.kwargs.get('standard_out', 'standard.out'))
         if self.mode == 'synth':
-            fout += f"synlimits\n{self.kwargs.get('synlimits', '5000.0 5050.0 0.01 1.')}\n"
-            fout += f"obspectrum   {self.kwargs.get('obspectrum', '5')}\n"
-            fout += f"plotpars     {self.kwargs.get('plotpars', '1')}\n"
-            fout += f"{self.kwargs.get('plotpars1', '5000.0 5050.0  0.00 1.02')}\n"
-            fout += f"{self.kwargs.get('plotpars2', '    0  0.00    0.00 0.00')}\n"
-            fout += f"{self.kwargs.get('plotpars3', '  r 0.0209  1.7  0.40  2.2  0.0')}\n"
-            fout += f"histogram    {self.kwargs.get('histogram', '1')}\n"
-            fout += f"smoothed_out {self.kwargs.get('smoothed_out', 'synth.asc')}\n"
-            fout += f"observed_in  {self.kwargs.get('observed_in', 'observed.asc')}\n"
-            fout += f"abundances   {self.kwargs.get('abundances', '1 3')}\n"
-            fout += f"   {self.kwargs.get('abundances1', '26   0.20   0.00   -0.20')}\n"
+            fout += "synlimits\n{}\n".format(self.kwargs.get('synlimits', '5000.0 5050.0 0.01 1.'))
+            fout += "obspectrum   {}\n".format(self.kwargs.get('obspectrum', '5'))
+            fout += "plotpars     {}\n".format(self.kwargs.get('plotpars', '1'))
+            fout += "{}\n".format(self.kwargs.get('plotpars1', '5000.0 5050.0  0.00 1.02'))
+            fout += "{}\n".format(self.kwargs.get('plotpars2', '    0  0.00    0.00 0.00'))
+            fout += "{}\n".format(self.kwargs.get('plotpars3', '  r 0.0209  1.7  0.40  2.2  0.0'))
+            fout += "histogram    {}\n".format(self.kwargs.get('histogram', '1'))
+            fout += "smoothed_out {}\n".format(self.kwargs.get('smoothed_out', 'synth.asc'))
+            fout += "observed_in  {}\n".format(self.kwargs.get('observed_in', 'observed.asc'))
+            fout += "abundances   {}\n".format(self.kwargs.get('abundances', '1 3'))
+            fout += "   {}\n".format(self.kwargs.get('abundances1', '26   0.20   0.00   -0.20'))
         else:
-            fout += f"strong      {self.kwargs.get('strong', '0')}\n"
-            fout += f"iraf        {self.kwargs.get('iraf', '0')}\n"
-            fout += f"opacit      {self.kwargs.get('opacit', '0')}\n"
-            fout += f"freeform    {self.kwargs.get('freeform', '0')}\n"
-            fout += f"obspectrum   {self.kwargs.get('obspectrum', '0')}\n"
+            fout += "strong      {}\n".format(self.kwargs.get('strong', '0'))
+            fout += "iraf        {}\n".format(self.kwargs.get('iraf', '0'))
+            fout += "opacit      {}\n".format(self.kwargs.get('opacit', '0'))
+            fout += "freeform    {}\n".format(self.kwargs.get('freeform', '0'))
+            fout += "obspectrum   {}\n".format(self.kwargs.get('obspectrum', '0'))
 
         with open('conf.par', 'w') as f:
             f.write(fout)
@@ -139,4 +139,4 @@ if __name__ == "__main__":
     }
     m = MOOG(mode='synth', verbose=True, **options)
     d = m.output
-    d.plot(labels=map(lambda s: f'[Fe/H]={s}',['0.20', '0.00', '-0.20']))
+    d.plot(labels=map(lambda s: '[Fe/H]={}'.format(s),['0.20', '0.00', '-0.20']))
